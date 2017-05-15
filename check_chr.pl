d7ca6851e5d636669a993a5346b3e449ed80b792
Kübra NARCI <kbrnrc@gmail.com>
2017-05-15 11:12:53 +0300

    Add files via upload

---
+++ check_chr.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+my $num_args = $#ARGV + 1;
+if ($num_args != 2) {
+    print "\nUsage: check_chr.pl input_file output_file\n";
+    exit;
+}
+
+my $input_file=$ARGV[0];
+my $output_file=$ARGV[1];
+
+open(OUTPUT,">",$output_file);
+
+my @array1;
+
+open (my $inFile, '<', $input_file) or die $!;
+
+while (<$inFile>) {
+	push (@array1, split /\n/);
+
+}
+
+close ($inFile);
+
+foreach my $el (@array1){
+
+	my @line = (split /\t/, $el);
+
+	if ($line[0] =~ /^chr/ ){
+		print $line[0];
+		print OUTPUT "$el\n";
+
+	}
+	else {print OUTPUT "chr$el\n";}
+
+}
+
+close OUTPUT;
